6a5a522dfe073d403ba29d27339cf6cb5d6c4bfd
AbbasBourji <abbas.bourji@hotmail.com>
2016-03-17 23:13:49 +0100

    Hallo World First Commit

--- index.android.js
+++ index.android.js
@@ -1,25 +1,35 @@
-import React, {
-  AppRegistry,
-  Component,
-  View,
-  Text,
-  StyleSheet,
-  TextInput
-} from 'react-native';
+import React, { AppRegistry, Component, Image, StyleSheet, Text, View, } from 'react-native';
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
+
+class MyFirstApp extends Component {
+  render() {
+    var movie = MOCKED_MOVIES_DATA[0];
+    return (
+      <View style={styles.container}>
+        <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>
+            {movie.title}
+          </Text>
+          <Text style={styles.year}>
+            {movie.year}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
 
 var styles = StyleSheet.create({
-});
-
-var MyFirstApp = React.createClass({
-  render: function() {
-    var layout =
-        <View>
-            <Text>
-            </Text>
-        </View>
-    ;
-	return layout;
-  }
+  container: { flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF', },
+  thumbnail: { width: 53, height: 81, },
+  rightContainer: { flex: 1, },
+  title: { fontSize: 20, marginBottom: 8, textAlign: 'center', },
+  year: { textAlign: 'center', },
 });
 
 AppRegistry.registerComponent('MyFirstApp', () => MyFirstApp);
