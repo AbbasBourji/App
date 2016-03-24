5939fbde8c56a19f4ec6223428728943870082a5
AbbasBourji <abbas.bourji@hotmail.com>
2016-03-25 00:09:27 +0100

    Dynamic Display and dynamic Style

--- index.android.js
+++ index.android.js
@@ -6,7 +6,7 @@ import React, {
   StyleSheet,
   TextInput
 } from 'react-native';
-
+var Moment = require('moment');
 var DayItem = require('./src/day-item.js');
 
 var DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
@@ -14,16 +14,24 @@ class MyFirstApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Days of the week</Text>
         {this.days()}
       </View>
     );
   }
 
   days() {
+    var daysItems = [];
+    for(var i=1; i<8; i++){
+      var day = Moment().add(i, 'days').format('dddd');
+      daysItems.push(
+        <DayItem day={day} daysUntil={i} />
+      )
+    }
+    return daysItems
+    /* map la length lel array
     return DAYS.map(function(day){
       return <DayItem day={day} />
-    });
+    });*/
   }
 
 };
--- src/day-item.js
+++ src/day-item.js
@@ -7,21 +7,47 @@ import React, {
   TextInput
 } from 'react-native';
 
+/* {this.props.day} bidta3mela la el2at el variable mn file la gheyr file     */
 class DayItem extends Component {
   render() {
     return (
-        <Text style={styles.day}>
+        <Text style={this.style()}>
           {this.props.day}
         </Text>
     );
   }
+  style(){
+    return {
+      color: this.color(),
+      //fontWeight: this.fontWeight(),
+      fontSize: this.fontSize(),
+      lineHeight: this.lineHeight(),
+    }
+  }
+  color(){
+
+    var opacity = 1 / this.props.daysUntil;
+    return 'rgba(0, 0, 0, '+ opacity + ')';
+    // rgba(0, 0, 0, .3)
+  }
+  fontWeight(){
+    var weight = 7 - this.props.daysUntil;
+    return weight * 100;
+  }
+  fontSize(){
+    return 60 - 6 * this.props.daysUntil;
+  }
+  lineHeight(){
+    return 70 - 4 * this.props.daysUntil;
+  }
 };
 
-var styles = StyleSheet.create({
+/*var styles = StyleSheet.create({
   day:{
     fontSize: 18,
     color: '#0000FF'
   }
-});
+});*/
 
+// lezim ektoub heyda el sater ka2eno return lel file
 module.exports = DayItem;
