4ef06da85f4cdbcc1dfbedbf0261d4abd1a4f8e3
AbbasBourji <abbas.bourji@hotmail.com>
2016-03-25 02:15:35 +0100

    Change button name and button style dynamic on button click

--- index.android.js
+++ index.android.js
@@ -12,28 +12,29 @@ var formatTime = require('minutes-seconds-milliseconds');
 var MyFirstApp = React.createClass({
   getInitialState: function(){
     return {
-      timeElapsed: null
+      timeElapsed: null,
+      running : false
     }
   },
   render: function() {
     return (
       <View style={styles.container}>
-        <View style={[styles.header, this.border('yellow')]}>
+        <View style={styles.header}>
 
-          <View style={[styles.timerWrapper, this.border('red')]}>
-            <Text>
+          <View style={styles.timerWrapper}>
+            <Text style={styles.timer}>
               {formatTime(this.state.timeElapsed)}
             </Text>
           </View>
 
-          <View style={[styles.buttonWrapper, this.border('green')]}>
+          <View style={styles.buttonWrapper}>
             {this.startStopButton()}
             {this.lapButton()}
           </View>
 
         </View>
 
-        <View style={[styles.footer, this.border('blue')]}>
+        <View style={styles.footer}>
           <Text>
             I a List of Laps
           </Text>
@@ -43,38 +44,47 @@ var MyFirstApp = React.createClass({
     );
   },
   startStopButton: function(){
+    var style = this.state.running ? styles.stopButton : styles.startButton;
     return <TouchableHighlight // to make the it clickable
       underlayColor="gray" // To change Color on Click
       onPress={this.handleStartPress} // Handling on click function
+      style={[styles.button, style]}
       >
         <Text>
-          Start
+          {this.state.running ? 'Stop' : 'Start'}
         </Text>
       </TouchableHighlight>
   },
   lapButton: function(){
-    return <View>
+    return <View style={styles.button}>
       <Text>
         Lap
       </Text>
     </View>
   },
   handleStartPress: function(){
+    if(this.state.running){ // if the Timer Running ?
+      clearInterval(this.interval);
+      this.setState({running: false});
+      return
+    }
+
     var startTime = new Date();
 
-    setInterval( () => { // () => function()
+    this.interval = setInterval( () => { // () => function()
       // Use to update the state with new Value
       this.setState({
-        timeElapsed: new Date() - startTime
+        timeElapsed: new Date() - startTime,
+        running: true
       });
     }, 30);
-  },
+  }/*,  // example this.border('red')
   border: function(color){
     return {
       borderColor: color,
       borderWidth: 4
     }
-  }
+  }*/
 });
 
 var styles = StyleSheet.create({
@@ -102,6 +112,23 @@ var styles = StyleSheet.create({
     flexDirection: 'row', // to make component Horizontal
     justifyContent: 'space-around', // to make the same space between Items and between Right/Left
     alignItems: 'center' // to make the items in Vertical Center
+  },
+  timer:{
+    fontSize: 60
+  },
+  button:{
+    borderWidth: 2,
+    height: 80,
+    width: 80,
+    borderRadius : 50, // Circle
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  startButton:{
+    borderColor: '#00CC00'
+  },
+  stopButton:{
+    borderColor: '#CC0000'
   }
 });
 
